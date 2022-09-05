b92918fa38f37b4ed5dbf3747ed545b9b8731706
Umar <gadaevduk@gmail.com>
2022-09-05 12:15:45 +0300

    backend

---
+++ first.js
@@ -0,0 +1,7 @@
+ function a(){
+    let count = 1;
+    return 1
+}
+ const b = 2;
+ const c = 3;
+ module.exports = {a, b, c}
---
+++ second.js
@@ -0,0 +1,3 @@
+let e = require("./first")
+
+console.log(e.b, e.a, e.c);
